dee99479a740afd4a35ba3a7dc1fe30acb2883b2
Andres Diaz <andres@finhubtechnologies.com>
2022-07-07 16:08:20 +0100

    todo-list project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="todoapp">
+    <TheHeader />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/the-header.vue'
+
+export default {
+  name: 'App',
+  components: { TheHeader }
+}
+</script>
+
+<style>
+.todoapp {
+  max-width: var(--max-width-sm);
+  margin: 130px auto 40px;
+  padding: var(--size-base-x-3);
+	background: #fff;
+	box-shadow: var(--box-shadow);
+}
+</style>
---
+++ src/components/the-header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="the-header">
+    <img alt="Vue logo" class="the-header__logo" src="@/assets/todo.png" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'  
+}
+</script>
+
+<style>
+.the-header__logo {
+  display: block;
+  margin: 0 auto;
+  width: 80px;
+}
+</style>
